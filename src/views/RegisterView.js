4ec262809dbac611c93fab824ac15af0cc84264c
arestus <arestus@gmail.com>
2021-04-16 23:44:19 +0300

    add hook for register

--- src/views/RegisterView.js
+++ src/views/RegisterView.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { authOperations } from '../redux/auth';
-
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -20,127 +18,89 @@ const styles = {
   },
 };
 
-class RegisterView extends Component {
-  state = {
+export default function RegisterView() {
+  const [newUser, setNewUser] = useState({
     name: '',
     email: '',
     password: '',
-  };
+  });
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
+  const dispatch = useDispatch();
 
-  handleSubmit = e => {
-    e.preventDefault();
+  const handleChange = useCallback(
+    ({ target: { name, value } }) => {
+      setNewUser({ ...newUser, [name]: value });
+    },
+    [newUser],
+  );
 
-    this.props.onRegister(this.state);
+  authOperations.register(newUser);
 
-    this.setState({ name: '', email: '', password: '' });
-  };
+  const handleSubmit = useCallback(
+    event => {
+      event.preventDefault();
 
-  render() {
-    const { name, email, password } = this.state;
+      dispatch(authOperations.register(newUser));
 
-    return (
-      <div>
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <h1>Страница регистрации</h1>
-          <Typography component="h1" variant="h5">
-            Регистрация
-          </Typography>
-          <form
-            onSubmit={this.handleSubmit}
-            style={styles.form}
-            autoComplete="off"
-          >
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Логин"
-              name="name"
-              autoComplete="login"
-              autoFocus
-              value={name}
-              onChange={this.handleChange}
-            />
-            {/* <label style={styles.label}>
-              Имя
-              <input
-                type="text"
-                name="name"
-                value={name}
-                onChange={this.handleChange}
-              />
-            </label> */}
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="E-mail"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={this.handleChange}
-            />
-            {/* <label style={styles.label}>
-              Почта
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={this.handleChange}
-              />
-            </label> */}
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="Пароль"
-              name="password"
-              autoComplete="current-password"
-              autoFocus
-              type="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-            {/* <label style={styles.label}>
-              Пароль
-              <input
-                type="password"
-                name="password"
-                value={password}
-                onChange={this.handleChange}
-              />
-            </label> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              // className={classes.submit}
-            >
-              Зарегистрироваться
-            </Button>
-            {/* <button type="submit">Зарегистрироваться</button> */}
-          </form>
-        </Container>
-      </div>
-    );
-  }
+      setNewUser({ name: '', email: '', password: '' });
+    },
+    [dispatch, newUser],
+  );
+
+  return (
+    <div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <h1>Страница регистрации</h1>
+        <Typography component="h1" variant="h5">
+          Регистрация
+        </Typography>
+        <form onSubmit={handleSubmit} style={styles.form} autoComplete="off">
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Логин"
+            name="name"
+            autoComplete="login"
+            autoFocus
+            value={newUser.name}
+            onChange={handleChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="E-mail"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={newUser.email}
+            onChange={handleChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="password"
+            label="Пароль"
+            name="password"
+            autoComplete="current-password"
+            autoFocus
+            type="password"
+            value={newUser.password}
+            onChange={handleChange}
+          />
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Зарегистрироваться
+          </Button>
+        </form>
+      </Container>
+    </div>
+  );
 }
-
-const mapDispatchToProps = {
-  onRegister: authOperations.register,
-};
-
-export default connect(null, mapDispatchToProps)(RegisterView);
